54d67028b9da1185c70ceccd7cf7112c4a829716
kchason <1111099+kchason@users.noreply.github.com>
2021-09-28 20:17:37 -0400

    Initial test for get_record_content

--- sqlite_dissect/tests/utilities_test.py
+++ sqlite_dissect/tests/utilities_test.py
@@ -1,7 +1,9 @@
+import struct
 import unittest
 
-from sqlite_dissect.utilities import calculate_expected_overflow, get_serial_type_signature, has_content
-from sqlite_dissect.constants import STORAGE_CLASS, BLOB_SIGNATURE_IDENTIFIER, TEXT_SIGNATURE_IDENTIFIER
+from sqlite_dissect.utilities import calculate_expected_overflow, get_serial_type_signature, has_content, \
+    get_record_content
+from sqlite_dissect.constants import BLOB_SIGNATURE_IDENTIFIER, TEXT_SIGNATURE_IDENTIFIER
 
 
 class TestRootUtilities(unittest.TestCase):
@@ -24,6 +26,90 @@ class TestRootUtilities(unittest.TestCase):
         result = calculate_expected_overflow(15, 10)
         self.assertEqual(3, result[0])  # overflow_pages
         self.assertEqual(3, result[1])  # last_overflow_page_content_size
+
+    def test_get_record_content(self):
+        test_string_array = list('this is a string')
+        test_string_array = 'this is a string'.encode('utf-8')
+        test_byte_array = struct.pack('=BHBL', 1, 2, 3, 4)
+
+        # Test when serial_type is 0
+        result = get_record_content(0, test_byte_array, 0)
+        self.assertEqual(0, result[0])
+        self.assertEqual(None, result[1])
+
+        # Test when serial_type is 1
+        result = get_record_content(1, test_byte_array, 0)
+        self.assertEqual(1, result[0])
+        # self.assertEqual(None, result[1])
+
+        # Test when serial_type is 2
+        result = get_record_content(2, test_byte_array, 0)
+        self.assertEqual(2, result[0])
+        # self.assertEqual(None, result[1])
+
+        # Test when serial_type is 3
+        result = get_record_content(3, test_byte_array, 0)
+        self.assertEqual(3, result[0])
+        # self.assertEqual(None, result[1])
+
+        # Test when serial_type is 4
+        result = get_record_content(4, test_byte_array, 0)
+        self.assertEqual(4, result[0])
+        # self.assertEqual(None, result[1])
+
+        # Test when serial_type is 5
+        result = get_record_content(5, test_byte_array, 0)
+        self.assertEqual(6, result[0])
+        # self.assertEqual(None, result[1])
+
+        # Test when serial_type is 6
+        result = get_record_content(6, test_byte_array, 0)
+        self.assertEqual(8, result[0])
+        # self.assertEqual(None, result[1])
+
+        # Test when serial_type is 7
+        result = get_record_content(7, test_byte_array, 0)
+        self.assertEqual(8, result[0])
+        # self.assertEqual(0, result[1])
+
+        # Test when serial_type is 8
+        result = get_record_content(8, test_byte_array, 0)
+        self.assertEqual(0, result[0])
+        self.assertEqual(0, result[1])
+
+        # Test when serial_type is 9
+        result = get_record_content(9, test_byte_array, 0)
+        self.assertEqual(0, result[0])
+        self.assertEqual(1, result[1])
+
+        # Test when serial_type is >= 12 and even
+        result = get_record_content(12, test_string_array, 0)
+        self.assertEqual(0, result[0])
+        self.assertEqual('', result[1])
+
+        result = get_record_content(24, test_string_array, 0)
+        self.assertEqual(6, result[0])
+        self.assertEqual('this i', result[1])
+
+        # Test when serial_type is >= 13 and odd
+        result = get_record_content(13, test_string_array, 0)
+        self.assertEqual(0, result[0])
+        self.assertEqual('', result[1])
+
+        result = get_record_content(25, test_string_array, 0)
+        self.assertEqual(6, result[0])
+        self.assertEqual('this i', result[1])
+
+        # Test that the proper exception is thrown when the input is invalid
+        cases = [10, 11]
+        for case in cases:
+            with self.assertRaises(Exception):
+                get_record_content(case, test_string_array, 0)
+
+        cases = [3.5, -1, 13.7]
+        for case in cases:
+            with self.assertRaises(ValueError):
+                get_record_content(case, test_string_array, 0)
 
     def test_get_serial_type_signature(self):
         # Test arguments that don't match and get overridden
