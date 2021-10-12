31d8da79e7a1de3beaf4cd3cafb6a8132a462e49
Keith Chason <kchason@users.noreply.github.com>
2021-10-12 09:56:36 -0400

    Initial stub for CASE export

---
+++ sqlite_dissect/export/case_export.py
@@ -0,0 +1,9 @@
+"""
+
+case_export.py
+
+This script holds the objects used for exporting information regarding SQLite carving process to CASE format. 
+Information about the CASE Cyber Ontology can be found at: https://caseontology.org/
+
+
+"""
