4bf6eedadf25e00dc61eae53d5e3ce8d53ac4aa7
kchason <kchason@users.noreply.github.com>
2022-01-10 09:39:22 -0500

    Add string docs for utlities

--- sqlite_dissect/utilities.py
+++ sqlite_dissect/utilities.py
@@ -17,6 +17,9 @@ utilities.py
 
 This script holds general utility functions for reference by the sqlite carving library.
 
+The script holds the following class(es):
+DotDict(dict)
+
 This script holds the following function(s):
 calculate_expected_overflow(overflow_byte_size, page_size)
 decode_varint(byte_array, offset)
